30ecb999090262c03db04a4e5edc2a62b2033c0b
phanhai27 <phanhai27@gmail.com>
2024-08-26 14:37:47 +0700

    setup and get location updates

--- app/src/main/java/cc/apollai/location_service/LocationForegroundService.kt
+++ app/src/main/java/cc/apollai/location_service/LocationForegroundService.kt
@@ -1,20 +1,36 @@
 package cc.apollai.location_service
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
 import android.content.pm.ServiceInfo
+import android.location.Location
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ServiceCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlin.time.Duration.Companion.seconds
 
 class LocationForegroundService: Service() {
     private val binder = LocalBinder()
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
+    private val locationFlow = MutableStateFlow<Location?>(null)
+
     companion object {
         private const val TAG = "LocationForegroundService"
+        private val LOCATION_UPDATES_INTERVAL_MS = 1.seconds.inWholeMilliseconds
     }
 
     inner class LocalBinder: Binder() {
@@ -30,6 +46,7 @@ class LocationForegroundService: Service() {
         Log.d(TAG, "onStartCommand")
 
         startAsForegroundService()
+        startLocationUpdates()
 
         return super.onStartCommand(intent, flags, startId)
     }
@@ -37,6 +54,8 @@ class LocationForegroundService: Service() {
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, "onCreate")
+
+        setupLocationUpdates()
 
         Toast.makeText(this, "Foreground Service created", Toast.LENGTH_SHORT).show()
     }
@@ -60,6 +79,26 @@ class LocationForegroundService: Service() {
             } else {
                 0
             }
+        )
+    }
+
+    private fun setupLocationUpdates() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    locationFlow.value = location
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(
+            LocationRequest.Builder(
+                LOCATION_UPDATES_INTERVAL_MS
+            ).build(), locationCallback, Looper.getMainLooper()
         )
     }
 }
